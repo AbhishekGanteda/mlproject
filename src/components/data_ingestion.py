3c8ef5dee14156fbd0750cdd88c39d4ec0a23fb9
AbhishekGanteda <rockingabhi2003@gmail.com>
2024-10-29 17:01:45 +0530

    Model Trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,8 @@ from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 from data_transformations import DataTransformation
 from data_transformations import DataTransformationConfig
+from model_trainer import ModelTrainerConfig
+from model_trainer import ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -16,7 +18,7 @@ class DataIngestionConfig:
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion__config=DataIngestionConfig()
+        self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion component")
@@ -25,29 +27,31 @@ class DataIngestion:
             df=pd.read_csv('notebook\data\stud.csv')
             logging.info("Read the dataset as dataframe")
             # make directory i.e artifacts
-            os.makedirs(os.path.dirname(self.ingestion__config.train_data_path),exist_ok=True)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
             # adding raw data to data.csv
-            df.to_csv(self.ingestion__config.raw_data_path,index=False,header=True) 
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True) 
 
             logging.info("train-test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             # adding training data to train.csv
-            train_set.to_csv(self.ingestion__config.train_data_path,index=False,header=True) 
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True) 
             # adding testing data to test.csv
-            test_set.to_csv(self.ingestion__config.test_data_path,index=False,header=True) 
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True) 
             
             logging.info("Ingestion of data is completed")
 
             return (
-                self.ingestion__config.train_data_path,
-                self.ingestion__config.test_data_path,
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path,
             )
         except Exception as e:
             raise CustomException(e,sys)
         
 if __name__=="__main__":
     obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+    model_trainer=ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
--- src/components/data_transformations.py
+++ src/components/data_transformations.py
@@ -23,30 +23,20 @@ class DataTransformation:
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
-        '''
-        This function si responsible for data trnasformation
-        
-        '''
         try:
             numerical_columns = ["writing_score", "reading_score"]
             categorical_columns = [
-                "gender",
-                "race_ethnicity",
-                "parental_level_of_education",
-                "lunch",
-                "test_preparation_course",
+                "gender","race_ethnicity","parental_level_of_education","lunch","test_preparation_course",
             ]
 
             num_pipeline= Pipeline(
                 steps=[
                 ("imputer",SimpleImputer(strategy="median")),
                 ("scaler",StandardScaler())
-
                 ]
             )
 
             cat_pipeline=Pipeline(
-
                 steps=[
                 ("imputer",SimpleImputer(strategy="most_frequent")),
                 ("one_hot_encoder",OneHotEncoder()),
@@ -62,10 +52,7 @@ class DataTransformation:
                 [
                 ("num_pipeline",num_pipeline,numerical_columns),
                 ("cat_pipelines",cat_pipeline,categorical_columns)
-
                 ]
-
-
             )
 
             return preprocessor
@@ -86,7 +73,6 @@ class DataTransformation:
             preprocessing_obj=self.get_data_transformer_object()
 
             target_column_name="math_score"
-            numerical_columns = ["writing_score", "reading_score"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
@@ -104,7 +90,6 @@ class DataTransformation:
             ]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
-            logging.info(f"Saved preprocessing object.")
 
             save_object(
 
@@ -112,6 +97,7 @@ class DataTransformation:
                 obj=preprocessing_obj
 
             )
+            logging.info(f"Saved preprocessing object.")
 
             return (
                 train_arr,
